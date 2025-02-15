969c1c12a3378fc5e4c1c87d933ac84e931085af
Brian Yin <byin@roblox.com>
2025-02-15 14:57:01 -0800

    add

--- agent-console/app/_components/console-container.tsx
+++ agent-console/app/_components/console-container.tsx
@@ -9,14 +9,13 @@ import { WideSwitch } from "@/components/wide-switch";
 import { getEventLevel, getEventMessage, useLogger } from "@/hooks/use-logger";
 import { EventType } from "@/lib/event-types";
 import { cn } from "@/lib/utils";
-import {
-  ControlBar as LivekitControlBar,
-  VoiceAssistantControlBar,
-} from "@livekit/components-react";
+import { ControlBar as LivekitControlBar, useConnectionState } from "@livekit/components-react";
+import { AnimatePresence, motion } from "framer-motion";
+import { ChevronDown } from "lucide-react";
 import { useEffect, useState } from "react";
 import { ConnectionButton } from "./connection-button";
-import { ControlBar } from "./control-bar";
 import { LevelFilter } from "./level-filter";
+import { LivekitStateTabs } from "./livekit-state-tabs";
 import { LogItem } from "./log-item";
 import { ParticipantViewer } from "./participant-viewer";
 import { RoomStateViewer } from "./room-state-viewer";
@@ -27,11 +26,14 @@ export const ConsoleContainer: React.FC<React.HTMLAttributes<HTMLDivElement>> = 
   const { logs, appendLog, clear } = useLogger();
   const [query, setQuery] = useState("");
   const [expandAll, setExpandAll] = useState<boolean>(false);
+  const [controlBarExpanded, setControlBarExpanded] = useState<boolean>(false);
 
   // Level filter
   const [displayInfo, setDisplayInfo] = useState(true);
   const [displayWarn, setDisplayWarn] = useState(true);
   const [displayError, setDisplayError] = useState(true);
+
+  const connectionState = useConnectionState();
 
   const numSelectedLevels = Object.values({
     displayInfo,
@@ -122,26 +124,79 @@ export const ConsoleContainer: React.FC<React.HTMLAttributes<HTMLDivElement>> = 
           <ResizableHandle withHandle />
           <ResizablePanel>
             <div className="h-full flex flex-col">
-              <div className="flex justify-between items-center p-4 border-b">
-                <div className="space-y-2 pr-3">
-                  <h2 className="text-lg font-bold">LiveKit in Real Time</h2>
-                  <p className="text-sm text-muted-foreground">
-                    Maximize the observability of your Livekit room.
-                  </p>
+              <div className="flex flex-col border-b p-4">
+                <div className="flex justify-between items-center">
+                  <div className="space-y-2 pr-3">
+                    <h2 className="text-lg font-bold">LiveKit in Real Time</h2>
+                    <p className="text-sm text-muted-foreground">
+                      Maximize the observability of your Livekit room.
+                    </p>
+                  </div>
+                  {/* Connection Button */}
+                  <div className="inline-flex -space-x-px divide-x divide-primary-foreground/30 rounded-lg shadow-sm shadow-black/5 rtl:space-x-reverse">
+                    <ConnectionButton className="rounded-none shadow-none first:rounded-s-lg last:rounded-e-lg focus-visible:z-10" />
+                    <Button
+                      className="rounded-none shadow-none first:rounded-s-lg last:rounded-e-lg focus-visible:z-10"
+                      size="icon"
+                      aria-label="Options"
+                      onClick={() => setControlBarExpanded(!controlBarExpanded)}
+                    >
+                      <ChevronDown
+                        size={16}
+                        strokeWidth={2}
+                        aria-hidden="true"
+                        className={cn(
+                          "transition-transform duration-300",
+                          controlBarExpanded ? "rotate-180" : ""
+                        )}
+                      />
+                    </Button>
+                  </div>
                 </div>
-                {/* Connection Button */}
-                <ConnectionButton />
+                <motion.div
+                  animate={{ height: controlBarExpanded ? "auto" : 0 }}
+                  initial={false}
+                  transition={{ duration: 0.3 }}
+                  className="overflow-hidden pt-4"
+                >
+                  <AnimatePresence mode="wait">
+                    {connectionState !== "disconnected" ? (
+                      <motion.div
+                        key="control-bar"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                      >
+                        <LivekitControlBar
+                          className="border bg-muted rounded-md"
+                          controls={{ leave: false }}
+                        />
+                      </motion.div>
+                    ) : (
+                      <motion.div
+                        key="no-connection"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className="flex items-center justify-center border bg-muted rounded-md p-4"
+                      >
+                        <p className="text-sm text-muted-foreground">
+                          No connection to LiveKit server
+                        </p>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </motion.div>
               </div>
-              <ControlBar className="m-4" />
+
+              {/* <ControlBar className="m-4" /> */}
+              {/* <ThemePicker /> */}
+              {/* <VoiceAssistantControlBar controls={{ leave: false }} /> */}
+              <LivekitStateTabs className="m-4" />
               <div className="flex-1 relative">
                 <div className="absolute inset-0">
                   <ScrollArea className="h-full px-4">
                     <div className="flex flex-col py-4 gap-4">
-                      <ObservableWrapper>
-                        {/* <ThemePicker /> */}
-                        <LivekitControlBar controls={{ leave: false }} />
-                        <VoiceAssistantControlBar controls={{ leave: false }} />
-                      </ObservableWrapper>
                       <ObservableWrapper>
                         <RoomStateViewer />
                       </ObservableWrapper>
